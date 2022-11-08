e0e1e1b58cbdaaf6b4c90762dc2ca77e5c3f3dec
Siva Prakash <prakashdeveloper@outlook.in>
2022-11-08 11:05:08 +0530

    Updated app.py, readme.md and requirements.txt files

--- app.py
+++ app.py
@@ -1,8 +1,72 @@
+import requests
+import pandas as pd
 import streamlit as st
+from bs4 import BeautifulSoup
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
 # setting app's title, icon & layout
-st.set_page_config(page_title="Page Title", page_icon="🎯")
+st.set_page_config(page_title="Github Scraper", page_icon="🎯")
 
+def scrape_data(user_name):
+    url = f"https://github.com/{user_name}?tab=repositories"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    info = {"name": soup.find(class_="vcard-fullname").get_text()}
+    info["image_url"] = soup.find(class_="avatar-user")["src"]
+    info["followers"] = (
+        soup.select_one("a[href*=followers]").get_text().strip().split("\n")[0]
+    )
+    info["following"] = (
+        soup.select_one("a[href*=following]").get_text().strip().split("\n")[0]
+    )
+
+    try:
+        info["location"] = soup.select_one("li[itemprop*=home]").get_text().strip()
+    except Exception:
+        info["location"] = ""
+
+    try:
+        info["url"] = soup.select_one("li[itemprop*=url]").get_text().strip()
+    except Exception:
+        info["url"] = ""
+
+    repositories = soup.find_all(class_="source")
+    repo_info = []
+    for repo in repositories:
+        try:
+            name = repo.select_one("a[itemprop*=codeRepository]").get_text().strip()
+            link = f"https://github.com/{user_name}/{name}"
+        except Exception:
+            name = ""
+            link = ""
+
+        try:
+            updated = repo.find("relative-time").get_text()
+        except Exception:
+            updated = ""
+
+        try:
+            language = repo.select_one("span[itemprop*=programmingLanguage]").get_text()
+        except Exception:
+            language = ""
+
+        try:
+            description = repo.select_one("p[itemprop*=description]").get_text().strip()
+        except Exception:
+            description = ""
+
+        repo_info.append(
+            {
+                "name": name,
+                "link": link,
+                "updated ": updated,
+                "language": language,
+                "description": description,
+            }
+        )
+    repo_info = pd.DataFrame(repo_info)
+    return info, repo_info
 
 def main():
     # display app header
--- readme.md
+++ readme.md
@@ -1,7 +1,34 @@
-# Streamlit template
-![Python](https://img.shields.io/badge/Python-0078D7.svg?logo=python&logoColor=white)
-![streamlit](https://img.shields.io/badge/Streamlit-FF4B4B?logo=Streamlit&logoColor=white)
-![terminal](https://img.shields.io/badge/Windows%20Terminal-4D4D4D?logo=windows%20terminal&logoColor=white)
-![vscode](https://img.shields.io/badge/Visual_Studio_Code-0078D4?logo=visual%20studio%20code&logoColor=white)
+# Github Scraper
+![made-with-python](https://img.shields.io/badge/Made%20with-Python-0078D4.svg)
+![pandas](https://img.shields.io/badge/Pandas-2C2D72?logo=pandas&logoColor=white)
+![streamlit](https://img.shields.io/badge/Streamlit-FF4B4B?logo=streamlit&logoColor=white)
+![terminal](https://img.shields.io/badge/Windows%20Terminal-4D4D4D?logo=Windows%20terminal&logoColor=white)
+![vscode](https://img.shields.io/badge/Visual_Studio_Code-0078D4?&logo=visual%20studio%20code&logoColor=white)
 
-This repository is a template repository which can used later for creating new streamlit app repositories containing all of the template repository's files and folders.
+Github scraper app is used to scrape public data for a specific github user profile. You can download the scraped data in `csv`, `json` and pandas profiling `html` report formats.
+
+## Installation
+Open command prompt and create new environment
+```
+conda create -n your_env_name python=<version>
+```
+Then Activate the newly created environment
+```
+conda activate your_env_name
+```
+Clone the repository using `git`
+```
+git clone https://github.com/Prakashdeveloper03/Github-Scraper.git
+```
+Change to the cloned directory
+```
+cd <directory_name>
+```
+Then install all requirement packages for the app
+```
+pip install -r requirements.txt
+```
+Then, Run the `app.py` script
+```
+streamlit run app.py
+```
